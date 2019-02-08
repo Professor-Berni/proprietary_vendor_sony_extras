caf5e6c402592104b6f94eccebb732c7a28ff9e0
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2019-02-08 18:56:52 +0100

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+Copyright (C) 2019 - The LineageOS Project
+
+These are extra kitakami-common files which are NOT part of system.ext4
+
+How to get the other non-open-source common files:
+==================================================
+
+- Fetch the latest firmware file (ftf) from xperifirm
+- Open it with (for example) ark
+- Extract system.sin
+- Open this file with flashtool's sin editor
+- Klick "Extract data"
+- Wait until finished
+- Create a sub-directory named system under /mnt:
+  sudo mkdir /mnt/system
+- Mount the file system.ext4:
+  sudo mount -o loop system.ext4 /mnt/system
+- Open a terminal window in device/sony/<name of device> (for example suzuran)
+- Create non-open-source file directories:
+  ./extract-files.sh /mnt > <name of device>.log (for example suzuran.log)
+- Control this log file for errors ("!! file not found in source")!!!
+  This should NOT happen... ;-)
+- If everything is fine, umount mounted system.ext4:
+  sudo umount /mnt/system
+
+- DONE
+
+2019-02-08 Bernhard Thoben
+
---
+++ kitakami-common-vendor-extras.mk
@@ -0,0 +1,22 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/sony/kitakami-common-extras/proprietary/bin/tad_static:system/bin/tad_static \
+    vendor/sony/kitakami-common-extras/proprietary/lib64/lib-preload64.so:system/lib64/lib-preload64.so \
+    vendor/sony/kitakami-common-extras/proprietary/vendor/lib/libaptXHD_encoder.so:system/vendor/lib/libaptXHD_encoder.so \
+    vendor/sony/kitakami-common-extras/proprietary/vendor/lib/libaptX_encoder.so:system/vendor/lib/libaptX_encoder.so \
+    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libaptXHD_encoder.so:system/vendor/lib64/libaptXHD_encoder.so \
+    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libaptX_encoder.so:system/vendor/lib64/libaptX_encoder.so
