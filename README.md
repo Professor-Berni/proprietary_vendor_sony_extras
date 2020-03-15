02b3d9c7dcfb733e668c28be31062a1b4a1ba0ec
Bernhard Thoben <berni-the-bear@web.de>
2020-03-15 15:33:47 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,29 +1,6 @@
-Copyright (C) 2019 - The LineageOS Project
+Copyright (C) 2020 - The LineageOS Project
 
 These are extra kitakami-common files which are NOT part of system.ext4
 OR
 which have been changed.
 
-How to get the other non-open-source common files:
-
-- Fetch the latest firmware file (ftf) from xperifirm
-- Open it with (for example) ark
-- Extract system.sin
-- Open this file with flashtool's sin editor
-- Klick "Extract data"
-- Wait until finished
-- Create a sub-directory named system under /mnt:
-  sudo mkdir /mnt/system
-- Mount the file system.ext4:
-  sudo mount -o loop system.ext4 /mnt/system
-- Open a terminal window in device/sony/name_of_device (for example suzuran)
-- Create non-open-source file directories:
-  ./extract-files.sh /mnt > name_of_device.log (for example suzuran.log)
-- Control this log file for errors ("!! file not found in source")!!!
-  This should NOT happen... ;-)
-- If everything is fine, umount mounted system.ext4:
-  sudo umount /mnt/system
-
-- DONE
-
-2019-02-19 Bernhard Thoben
--- kitakami-common-vendor-extras.mk
+++ kitakami-common-vendor-extras.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,17 +14,7 @@
 # limitations under the License.
 
 PRODUCT_COPY_FILES += \
-    vendor/sony/kitakami-common-extras/proprietary/bin/tad_static:system/bin/tad_static \
-    vendor/sony/kitakami-common-extras/proprietary/lib64/lib-preload64.so:system/lib64/lib-preload64.so \
-    vendor/sony/kitakami-common-extras/proprietary/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
     vendor/sony/kitakami-common-extras/proprietary/vendor/lib/libaptXHD_encoder.so:system/vendor/lib/libaptXHD_encoder.so \
     vendor/sony/kitakami-common-extras/proprietary/vendor/lib/libaptX_encoder.so:system/vendor/lib/libaptX_encoder.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libaptXHD_encoder.so:system/vendor/lib64/libaptXHD_encoder.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libaptX_encoder.so:system/vendor/lib64/libaptX_encoder.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib/libcneapiclient.so:system/vendor/lib/libcneapiclient.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib/libwqe.so:system/vendor/lib/libwqe.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libcneapiclient.so:system/vendor/lib64/libcneapiclient.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libprotobuf-cpp-N.so:system/vendor/lib64/libprotobuf-cpp-N.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libsettings.so:system/vendor/lib64/libsettings.so \
-    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libwqe.so:system/vendor/lib64/libwqe.so
+    vendor/sony/kitakami-common-extras/proprietary/vendor/lib64/libaptX_encoder.so:system/vendor/lib64/libaptX_encoder.so
